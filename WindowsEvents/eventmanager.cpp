ad88d495b840f6e0be48f8f62c618f272b819cd7
Maxim <progon3@mail.ru>
2015-10-30 22:02:37 +0200

    Update eventmanager.cpp

--- WindowsEvents/eventmanager.cpp
+++ WindowsEvents/eventmanager.cpp
@@ -1,54 +1,68 @@
 #include <iostream>
 #include <iomanip>
-#include <string>
-#include <windows.h>
-
+#include <cmath>
+#include <algorithm>
 #include "eventmanager.h"
 
-const int BUFFER_SIZE = 20000;
-const std::string EVENT_TYPE[] = {"", "ERROR_EVENT", "WARNING_EVENT", "", "INFORMATION_EVENT"};
 
-void Event_Manager::showEvents()
+const int BUFFER_SIZE = 20000;
+
+
+void EventManager::getEvent(const std::string& logName, EVENT_TYPE type)
 {
     HANDLE hEventLog;
-    LPCTSTR logName = L"Application";
     EVENTLOGRECORD* eventInfo;
     DWORD noteSize, nextNoteSize, eventLogCounter = 0;
     BYTE buffer[BUFFER_SIZE];
 
 
-    hEventLog = OpenEventLog(NULL, logName);
-    if(logName == NULL)
+    hEventLog = OpenEventLog(NULL, std::wstring(logName.begin(), logName.end()).c_str());
+    if(hEventLog == NULL)
         std::cout << "Could not open event log!";
 
     eventInfo = (EVENTLOGRECORD*)&buffer;
     GetOldestEventLogRecord(hEventLog, &eventLogCounter);
 
-    std::cout << eventLogCounter;
 
     while(ReadEventLog(hEventLog, EVENTLOG_FORWARDS_READ | EVENTLOG_SEQUENTIAL_READ,
                        0, eventInfo, BUFFER_SIZE, &noteSize, &nextNoteSize))
     {
         while(noteSize > 0)
         {
-//            std::cout << eventLogCounter++ << "   EVENT ID: " << std::setw(12) << eventInfo->EventID
-//                      << "  EVENT TYPE: "  << eventInfo->EventType << "  EVENT SOURCE: "
-//                      << (char*)((LPBYTE)eventInfo + sizeof(EVENTLOGRECORD)) << std::endl;
-
-//            std::cout << eventLogCounter++ << "   Event ID: " << std::setw(12) << eventInfo->EventID
-//                      << "  EventType: "  << EVENT_TYPE[eventInfo->EventType] << "  Source: "
-//                      << (char*)((LPBYTE)eventInfo + sizeof(EVENTLOGRECORD)) << std::endl;
-
-            for(int i = 0; i < 20; ++i)
-                std::cout << (char*)((LPBYTE)eventInfo + sizeof(EVENTLOGRECORD) + i);
-            std::cout << std::endl;
+            // Show event that contains a specified type.
+            if(eventInfo->EventType & type)
+                showEvent(eventInfo);
 
             noteSize -= eventInfo->Length;
-            eventInfo = (EVENTLOGRECORD*)((LPBYTE)eventInfo + eventInfo->Length);
+            eventInfo = (EVENTLOGRECORD*)((LPBYTE)eventInfo + eventInfo->Length); // Next record.
 
         }
         eventInfo = (EVENTLOGRECORD*)&buffer;
     }
+    CloseHandle(hEventLog);
+}
+
+//----------------------------------------------------
+
+void EventManager::showEvent(const EVENTLOGRECORD* record)
+{
+    static const std::string eventName[] = {"Error", "Warning", "Information",
+                                            "Success/Failure Audit"};
+    int nameIndex = log(record->EventType) / log(2);
+
+    std::wstring sourceName(reinterpret_cast<const wchar_t*>(
+                                reinterpret_cast<const unsigned char*>(record) + sizeof(EVENTLOGRECORD)));
+
+    std::cout << "Event ID: " << std::setw(12) << record->EventID << "  EventType: " << eventName[nameIndex]
+              << "  Source: " << std::string(sourceName.begin(), sourceName.end()) << std::endl;
+}
+
+//----------------------------------------------------
+
+void EventManager::run()
+{
+    getEvent("Application", EVENTLOG_ERROR_TYPE | EVENTLOG_WARNING_TYPE);
+    getEvent("System", EVENTLOG_ERROR_TYPE | EVENTLOG_WARNING_TYPE);
 }
 
 
