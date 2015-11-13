a404a71a86ee3e1960150dba0960abe64bcc2d3b
Maxim <progon3@mail.ru>
2015-11-13 19:51:47 +0200

    Create RegCleaner.h

---
+++ ScanWidgets/RegCleaner.h
@@ -0,0 +1,15 @@
+#ifndef REGCLEANER_H
+#define REGCLEANER_H
+
+#include <QWidget>
+
+class RegCleaner : public QWidget
+{
+    Q_OBJECT
+
+public:
+    RegCleaner(QWidget *parent = 0);
+    //~RegCleaner();
+};
+
+#endif // REGCLEANER_H
