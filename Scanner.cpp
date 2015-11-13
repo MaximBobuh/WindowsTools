8a78429b91ccb1e17b16019472c457a6bd203f9f
Maxim <progon3@mail.ru>
2015-11-13 19:57:34 +0200

    Create Scanner.cpp

---
+++ Scanner.cpp
@@ -0,0 +1,59 @@
+#include <vector>
+#include <thread>
+#include <chrono>
+#include <QDebug>
+#include <QApplication>
+#include "Scanner.h"
+
+Scanner::Scanner(ScanAlgorithm* scanAlg) : algorithm(scanAlg), dialog(0)
+{
+    connect(&sWindow, SIGNAL(signalStartScan()), this, SLOT(slotStartScan()));
+
+    sWindow.addTreeItems(SECTION_TREE, "Section to Scan", scanAlg->getScanSection());
+
+    sWindow.setWindowTitle(algorithm->NAME);
+    sWindow.show();
+}
+
+//-------------------------------------------------
+
+void Scanner::slotStartScan()
+{
+    std::vector<int> section_index;
+    sWindow.selectedSections(section_index, 0);
+
+    if(!section_index.empty())
+    {
+        if(dialog)
+            delete dialog;
+        dialog = new DialogScan(section_index.size());
+        dialog->show();
+
+        const QStringList sections = algorithm->getScanSection();
+
+        for(auto index : section_index)
+        {
+            QStringList result;
+            algorithm->scanSection(index, result);
+            analysisNotes(result);
+            if(result.size() != 0)
+                sWindow.addTreeItems(RESULT_TREE, sections.at(index), result);
+            dialog->setNextValue();
+        }
+        delete dialog;
+    }
+}
+
+//-------------------------------------------------
+
+void Scanner::analysisNotes(const QStringList& list)
+{
+    for(auto str : list)
+    {
+        QApplication::processEvents();
+        dialog->setInfo(str);
+        dialog->setNextFound();
+    }
+}
+
+//-------------------------------------------------
